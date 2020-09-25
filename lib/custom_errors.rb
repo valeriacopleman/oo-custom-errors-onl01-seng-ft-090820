6a58678c4ce10c459027e1b612a47560cd894bb8
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-25 15:48:58 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -17,6 +17,10 @@ class Person
     person.partner = self
   end
 end
+
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
   
   class PartnerError < StandardError
     
